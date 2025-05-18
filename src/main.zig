5b3c635b958a88ac9cf3f86f96cffebb05999346
unlomtrois <vadim.kuz02@gmail.com>
2025-05-18 21:23:38 +0300

    refactor: move stripUtf8Bom function to bom.zig and update main.zig to use it

--- src/main.zig
+++ src/main.zig
@@ -3,44 +3,10 @@
 //! is to delete this file and start with root.zig instead.
 
 const std = @import("std");
+const bom = @import("bom.zig");
 
-/// Returns the slice with a leading UTF-8 BOM (EF BB BF) stripped off.
-/// If there is no BOM the original slice is returned unchanged.
-inline fn stripUtf8Bom(bytes: []const u8) []const u8 {
-    if (bytes.len >= 3 and
-        bytes[0] == 0xEF and
-        bytes[1] == 0xBB and
-        bytes[2] == 0xBF)
-    {
-        return bytes[3..];
-    }
-    return bytes;
-}
-
-test "stripUtf8Bom bom" {
-    const raw_bytes = [_]u8{ 0xEF, 0xBB, 0xBF, 'l', 'o', 'l' };
-
-    const bytes: []const u8 = stripUtf8Bom(&raw_bytes);
-
-    try std.testing.expectEqual(bytes[0], 'l');
-    try std.testing.expectEqual(bytes[1], 'o');
-    try std.testing.expectEqual(bytes[2], 'l');
-}
-
-test "stripUtf8Bom no bom" {
-    const raw_bytes = [_]u8{ 'l', 'o', 'l' };
-
-    const bytes: []const u8 = stripUtf8Bom(&raw_bytes);
-
-    try std.testing.expectEqual(bytes[0], 'l');
-    try std.testing.expectEqual(bytes[1], 'o');
-    try std.testing.expectEqual(bytes[2], 'l');
-}
-
-test "stripUtf8Bom bom and empty" {
-    const raw_bytes = [_]u8{ 0xEF, 0xBB, 0xBF };
-    const bytes: []const u8 = stripUtf8Bom(&raw_bytes);
-    try std.testing.expect(bytes.len == 0);
+fn openFile(filepath: []const u8) std.fs.File.OpenError!std.fs.File {
+    return std.fs.cwd().openFile(filepath, .{ .mode = .read_only });
 }
 
 pub fn main() !void {
@@ -53,11 +19,12 @@ pub fn main() !void {
 
     // const exename = args[0];
     // args = args[1..];
-
     // try std.io.getStdOut().writer().print("{s}\n", .{exename});
+
     const stdout = std.io.getStdOut().writer();
 
-    const file = try std.fs.cwd().openFile("kek.txt", .{});
+    const filepath = "kek.txt";
+    const file = try openFile(filepath);
     defer file.close();
 
     // todo: optimize using buffered reader
@@ -65,13 +32,11 @@ pub fn main() !void {
     defer allocator.free(raw_bytes);
     std.debug.assert(std.unicode.utf8ValidateSlice(raw_bytes));
 
-    const bytes = stripUtf8Bom(raw_bytes);
+    const bytes = bom.stripUtf8Bom(raw_bytes);
 
-    const view = try std.unicode.Utf8View.init(bytes);
-    var iterator = view.iterator();
-
-    while (iterator.nextCodepoint()) |codepoint| {
-        // '{c}' formats a u21 (Unicode scalar value) back to UTF-8
-        try stdout.print("{u}\n", .{codepoint});
+    var utf8 = (try std.unicode.Utf8View.init(bytes)).iterator();
+    while (utf8.nextCodepointSlice()) |codepoint| {
+        try stdout.print("{s}", .{codepoint});
     }
+    try stdout.writeByte('\n');
 }
