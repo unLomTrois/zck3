66f9be5b10e5792b36e473b6eff69a552adb1aa7
unlomtrois <vadim.kuz02@gmail.com>
2025-06-19 16:24:13 +0300

    refactor: improve formatting of optimization options in build.zig for better readability

--- build.zig
+++ build.zig
@@ -5,7 +5,9 @@ const std = @import("std");
 // runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .Debug });
+    const optimize = b.standardOptimizeOption(.{
+        .preferred_optimize_mode = .Debug,
+    });
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
