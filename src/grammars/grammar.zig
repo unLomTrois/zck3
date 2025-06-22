05bd1d520d76446fe71f9c04987a294ad44259c4
unlomtrois <vadim.kuz02@gmail.com>
2025-06-22 21:05:44 +0300

    refactor: move GrammarValidator to sep file

--- src/grammars/grammar.zig
+++ src/grammars/grammar.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub const Symbol = @import("symbol.zig").Symbol;
 pub const Rule = @import("rules.zig").Rule;
 pub const examples = @import("examples.zig");
+pub const validator = @import("validator.zig");
 
 /// Grammar is a deterministic context-free grammar. Written in Backus-Naur form.
 /// The purpose of Grammar is to define a set of production rules,
@@ -51,6 +52,15 @@ pub const Grammar = struct {
             allocator.free(self.rules[0].rhs);
         }
         allocator.free(self.rules);
+    }
+
+    pub fn asStatic(self: *const Grammar) StaticGrammar {
+        return StaticGrammar{
+            .start_symbol = self.start_symbol,
+            .terminals = self.terminals,
+            .non_terminals = self.non_terminals,
+            .rules = self.rules,
+        };
     }
 };
 
@@ -104,8 +114,8 @@ pub const GrammarBuilder = struct {
     }
 
     /// Returns a new static grammar. View does not own anything.
-    pub fn View(self: *const GrammarBuilder) Grammar {
-        return Grammar{
+    pub fn View(self: *const GrammarBuilder) StaticGrammar {
+        return StaticGrammar{
             .start_symbol = self.start_symbol,
             .terminals = self.terminals.items,
             .non_terminals = self.non_terminals.items,
@@ -213,128 +223,13 @@ test "expression grammar" {
     std.log.info("augmented grammar:\n{any}\n", .{augmented_grammar});
 }
 
-pub const GrammarError = error{
-    /// The start symbol was not found in the rules.
-    /// E.g. the start symbol is S, but there is no rule that starts with S.
-    StartSymbolNotFoundInRules,
+test "grammar validation" {
+    const allocator = std.testing.allocator;
+    const grammar = try examples.ExpressionGrammar(allocator);
+    defer grammar.deinit(allocator);
 
-    /// The start symbol is not a non-terminal.
-    StartSymbolIsNotNonTerminal,
-
-    EmptyTerminals,
-    EmptyNonTerminals,
-    EmptyRules,
-
-    DuplicateTerminal,
-    DuplicateNonTerminal,
-    OverlapBetweenSets,
-
-    LhsIsTerminal,
-    LhsIsNotNonTerminal,
-    UnknownSymbolInRhs,
-
-    UnreachableNonTerminal,
-    NonProductiveNonTerminal,
-} || std.mem.Allocator.Error; // OutOfMemory
-
-pub const GrammarValidator = struct {
-    const Self = @This();
-
-    fn validate(grammar: *const StaticGrammar) GrammarError!void {
-        try Self.validate_sets(grammar);
-        try Self.validate_start_symbol(grammar);
-    }
-
-    fn validate_sets(grammar: *const StaticGrammar) error{
-        EmptyTerminals,
-        EmptyNonTerminals,
-        EmptyRules,
-    }!void {
-        if (grammar.terminals.len == 0) {
-            return GrammarError.EmptyTerminals;
-        }
-        if (grammar.non_terminals.len == 0) {
-            return GrammarError.EmptyNonTerminals;
-        }
-        if (grammar.rules.len == 0) {
-            return GrammarError.EmptyRules;
-        }
-    }
-
-    fn validate_start_symbol(grammar: *const StaticGrammar) error{
-        StartSymbolNotFoundInRules,
-        StartSymbolIsNotNonTerminal,
-    }!void {
-        // First make sure at least one rule has the start symbol on the LHS.
-        const found_in_rules = blk: {
-            for (grammar.rules) |rule| {
-                if (rule.lhs.eql(grammar.start_symbol)) {
-                    break :blk true;
-                }
-            }
-            break :blk false;
-        };
-
-        if (!found_in_rules) {
-            return GrammarError.StartSymbolNotFoundInRules;
-        }
-
-        const found_in_non_terminals = blk: {
-            for (grammar.non_terminals) |non_terminal| {
-                if (non_terminal.eql(grammar.start_symbol)) {
-                    break :blk true;
-                }
-            }
-            break :blk false;
-        };
-
-        if (!found_in_non_terminals) {
-            return GrammarError.StartSymbolIsNotNonTerminal;
-        }
-    }
-};
-
-test "GrammarError.StartSymbolNotFoundInRules" {
-    const S = Symbol.from("S");
-    const A = Symbol.from("A");
-    const a = Symbol.from("a");
-
-    const failing_grammar = StaticGrammar.from(
-        S,
-        &.{a},
-        &.{ S, A },
-        &.{
-            // Rule.from(S, &.{A}), // This is missing
-            Rule.from(A, &.{a}),
-        },
-    );
-
-    GrammarValidator.validate(&failing_grammar) catch |err| {
-        try std.testing.expectEqual(GrammarError.StartSymbolNotFoundInRules, err);
-        return;
-    };
-}
-
-test "GrammarError.StartSymbolIsNotNonTerminal" {
-    const S = Symbol.from("S");
-    const A = Symbol.from("A");
-    const a = Symbol.from("a");
-
-    const failing_grammar = StaticGrammar.from(
-        S,
-        &.{a},
-        &.{
-            // S, // This is missing
-            A,
-        },
-        &.{
-            Rule.from(S, &.{ A, A }),
-            Rule.from(A, &.{a}),
-        },
-    );
-
-    GrammarValidator.validate(&failing_grammar) catch |err| {
-        try std.testing.expectEqual(GrammarError.StartSymbolIsNotNonTerminal, err);
-        return;
+    validator.GrammarValidator.validate(&grammar.asStatic()) catch |err| {
+        std.log.err("grammar validation failed: {any}\n", .{err});
+        unreachable; // No error for a valid grammar.
     };
 }
