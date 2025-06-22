5f8bdf156e153ab9e5e1a3933cc086ccad897daf
unlomtrois <vadim.kuz02@gmail.com>
2025-06-22 14:15:36 +0300

    refactor: introduce fromAlloc method for dynamic symbol creation and enhance RuleLike struct with allocation support; add tests for symbol allocation and out-of-scope scenarios

--- src/grammars/symbol.zig
+++ src/grammars/symbol.zig
@@ -4,8 +4,18 @@ pub const Symbol = struct {
     name: []const u8,
 
     /// Inline wrapper for symbol creation
+    /// deprecated: use fromAlloc instead
     pub inline fn from(name: []const u8) Symbol {
         return Symbol{ .name = name };
+    }
+
+    /// fromAlloc creates a symbol from a passed string, but also allocates the string inside the symbol
+    /// It returns an unmanaged symbol, caller is responsible for freeing the string.
+    /// Generally, you would use arena allocator for all three: grammar, rule, and symbol allocation.
+    /// This is useful for creating symbols that are not known at compile time, but are known at runtime
+    /// E.g. when parsing a grammar from a file
+    pub fn fromAlloc(alloc: std.mem.Allocator, name: []const u8) !Symbol {
+        return Symbol{ .name = try alloc.dupe(u8, name) };
     }
 
     /// Formats the struct as a string into a writer.
@@ -53,4 +63,76 @@ test "symbol_hash" {
     const symbol = Symbol.from("S");
     const symbol2 = Symbol.from("S");
     try std.testing.expect(symbol.eqlHash(symbol2));
+}
+
+test "symbol_allocFrom" {
+    const symbol = try Symbol.fromAlloc(std.testing.allocator, "S");
+    defer std.testing.allocator.free(symbol.name);
+    try std.testing.expectEqualStrings(symbol.name, "S");
+}
+
+fn outOfScope(alloc: std.mem.Allocator) !Symbol {
+    const S = try Symbol.fromAlloc(alloc, "S");
+
+    return S;
+}
+
+test "symbol_allocOutOfScope" {
+    const alloc = std.testing.allocator;
+    const symbol = try outOfScope(alloc);
+    defer alloc.free(symbol.name);
+
+    try std.testing.expectEqualStrings("S", symbol.name);
+}
+
+const RuleLike = struct {
+    lhs: Symbol,
+    rhs: []const Symbol,
+
+    fn from(lhs: Symbol, rhs: []const Symbol) RuleLike {
+        return RuleLike{ .lhs = lhs, .rhs = rhs };
+    }
+
+    fn fromAlloc(alloc: std.mem.Allocator, lhs: Symbol, rhs: []const Symbol) !RuleLike {
+        return RuleLike{ .lhs = lhs, .rhs = try alloc.dupe(Symbol, rhs) };
+    }
+};
+
+fn not_failingOutOfScope(alloc: std.mem.Allocator) !RuleLike {
+    const S = try Symbol.fromAlloc(alloc, "S");
+    const A = try Symbol.fromAlloc(alloc, "A");
+    const B = try Symbol.fromAlloc(alloc, "B");
+
+    return try RuleLike.fromAlloc(alloc, S, &.{ A, B });
+}
+
+test "not failingOutOfScope" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    const symbol_wrapper = try not_failingOutOfScope(alloc);
+
+    try std.testing.expectEqualStrings("A", symbol_wrapper.rhs[0].name);
+    try std.testing.expectEqualStrings("B", symbol_wrapper.rhs[1].name); // NOT SEGFAULT
+}
+
+fn symbols(alloc: std.mem.Allocator) ![]const Symbol {
+    const S = try Symbol.fromAlloc(alloc, "S");
+    const A = try Symbol.fromAlloc(alloc, "A");
+    const B = try Symbol.fromAlloc(alloc, "B");
+
+    return try alloc.dupe(Symbol, &.{ S, A, B });
+}
+
+test "symbols" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    const syms = try symbols(alloc);
+
+    for (syms) |sym| {
+        std.debug.print("{s}\n", .{sym.name});
+    }
 }
