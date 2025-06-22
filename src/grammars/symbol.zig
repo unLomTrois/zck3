1b642a6effb94453312366955c6734e56fa62633
unlomtrois <vadim.kuz02@gmail.com>
2025-06-22 14:43:54 +0300

    refactor: enhance Symbol struct with memory management and equality methods; update tests for symbol creation and out-of-scope scenarios

--- src/grammars/symbol.zig
+++ src/grammars/symbol.zig
@@ -3,8 +3,10 @@ const std = @import("std");
 pub const Symbol = struct {
     name: []const u8,
 
-    /// Inline wrapper for symbol creation
+    /// Inline wrapper for literal symbol creation
     /// deprecated: use fromAlloc instead
+    /// prefer fromAlloc for all cases, it's more explicit and safer.
+    /// Inlined symbols may not outlive the scope they are created in.
     pub inline fn from(name: []const u8) Symbol {
         return Symbol{ .name = name };
     }
@@ -18,6 +20,10 @@ pub const Symbol = struct {
         return Symbol{ .name = try alloc.dupe(u8, name) };
     }
 
+    pub fn deinit(self: *const Symbol, alloc: std.mem.Allocator) void {
+        alloc.free(self.name);
+    }
+
     /// Formats the struct as a string into a writer.
     /// E.g. std.fmt.allocPrint, std.io.getStdOut().writer(), etc.
     /// Not intended to be used directly. Instead provide symbol into args of std.fmt.allocPrint, etc.
@@ -28,63 +34,85 @@ pub const Symbol = struct {
         try writer.print("{s}", .{self.name});
     }
 
-    pub fn eql(self: *const Symbol, other: Symbol) bool {
-        return std.mem.eql(u8, self.name, other.name);
+    /// eql compares two symbols by their name.
+    pub fn eql(a: Symbol, b: Symbol) bool {
+        return std.mem.eql(u8, a.name, b.name);
     }
 
-    pub fn hash(self: *const Symbol) u64 {
-        return std.hash.RapidHash.hash(0, self.name);
-    }
-
-    pub fn eqlHash(self: *const Symbol, other: Symbol) bool {
-        return self.hash() == other.hash();
+    pub fn eqlTo(self: *const Symbol, other: Symbol) bool {
+        return self.eql(other);
     }
 };
 
 test "symbol_from" {
-    const symbol = Symbol.from("S");
+    const symbol = try Symbol.fromAlloc(std.testing.allocator, "S");
+    defer symbol.deinit(std.testing.allocator);
     try std.testing.expectEqualStrings(symbol.name, "S");
 }
 
 test "symbol_format" {
-    const symbol = Symbol.from("S");
+    const symbol = try Symbol.fromAlloc(std.testing.allocator, "S");
+    defer symbol.deinit(std.testing.allocator);
+
     const str = try std.fmt.allocPrint(std.testing.allocator, "{s}", .{symbol});
     defer std.testing.allocator.free(str);
+
     try std.testing.expectEqualStrings(str, "S");
 }
 
 test "symbol_eql" {
-    const symbol1 = Symbol.from("S");
-    const symbol2 = Symbol.from("S");
-    try std.testing.expect(symbol1.eql(symbol2));
+    const symbol1 = try Symbol.fromAlloc(std.testing.allocator, "S");
+    defer symbol1.deinit(std.testing.allocator);
+
+    const symbol2 = try Symbol.fromAlloc(std.testing.allocator, "S");
+    defer symbol2.deinit(std.testing.allocator);
+
+    try std.testing.expect(Symbol.eql(symbol1, symbol2)); // equivalent to:
+    try std.testing.expect(symbol1.eqlTo(symbol2));
 }
 
-test "symbol_hash" {
-    const symbol = Symbol.from("S");
-    const symbol2 = Symbol.from("S");
-    try std.testing.expect(symbol.eqlHash(symbol2));
-}
-
-test "symbol_allocFrom" {
+test "symbol_fromAlloc" {
     const symbol = try Symbol.fromAlloc(std.testing.allocator, "S");
-    defer std.testing.allocator.free(symbol.name);
+    defer symbol.deinit(std.testing.allocator);
+
     try std.testing.expectEqualStrings(symbol.name, "S");
 }
 
-fn outOfScope(alloc: std.mem.Allocator) !Symbol {
+/// Caller is responsible for freeing the symbol.
+fn outOfScopeSymbol(alloc: std.mem.Allocator) !Symbol {
     const S = try Symbol.fromAlloc(alloc, "S");
 
     return S;
 }
 
-test "symbol_allocOutOfScope" {
-    const alloc = std.testing.allocator;
-    const symbol = try outOfScope(alloc);
-    defer alloc.free(symbol.name);
+test "out of scope symbol" {
+    const symbol = try outOfScopeSymbol(std.testing.allocator);
+    defer symbol.deinit(std.testing.allocator);
 
     try std.testing.expectEqualStrings("S", symbol.name);
 }
 
+fn outOfScopeSlice(alloc: std.mem.Allocator) ![]const Symbol {
+    const S = try Symbol.fromAlloc(alloc, "S");
+    const A = try Symbol.fromAlloc(alloc, "A");
+    const B = try Symbol.fromAlloc(alloc, "B");
+
+    return try alloc.dupe(Symbol, &.{ S, A, B });
+}
+
+test "out of scope slice" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    const syms = try outOfScopeSlice(alloc);
+
+    for (syms) |sym| {
+        std.debug.print("{s}\n", .{sym.name});
+    }
+}
+
+// TODO: move following tests to rules.zig
 const RuleLike = struct {
     lhs: Symbol,
     rhs: []const Symbol,
@@ -98,7 +126,7 @@ const RuleLike = struct {
     }
 };
 
-fn not_failingOutOfScope(alloc: std.mem.Allocator) !RuleLike {
+fn outOfScopeRule(alloc: std.mem.Allocator) !RuleLike {
     const S = try Symbol.fromAlloc(alloc, "S");
     const A = try Symbol.fromAlloc(alloc, "A");
     const B = try Symbol.fromAlloc(alloc, "B");
@@ -106,33 +134,14 @@ fn not_failingOutOfScope(alloc: std.mem.Allocator) !RuleLike {
     return try RuleLike.fromAlloc(alloc, S, &.{ A, B });
 }
 
-test "not failingOutOfScope" {
+test "not failing out of scope rule" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const alloc = arena.allocator();
 
-    const symbol_wrapper = try not_failingOutOfScope(alloc);
+    const symbol_wrapper = try outOfScopeRule(alloc);
 
+    try std.testing.expectEqualStrings("S", symbol_wrapper.lhs.name);
     try std.testing.expectEqualStrings("A", symbol_wrapper.rhs[0].name);
     try std.testing.expectEqualStrings("B", symbol_wrapper.rhs[1].name); // NOT SEGFAULT
-}
-
-fn symbols(alloc: std.mem.Allocator) ![]const Symbol {
-    const S = try Symbol.fromAlloc(alloc, "S");
-    const A = try Symbol.fromAlloc(alloc, "A");
-    const B = try Symbol.fromAlloc(alloc, "B");
-
-    return try alloc.dupe(Symbol, &.{ S, A, B });
-}
-
-test "symbols" {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const alloc = arena.allocator();
-
-    const syms = try symbols(alloc);
-
-    for (syms) |sym| {
-        std.debug.print("{s}\n", .{sym.name});
-    }
 }
